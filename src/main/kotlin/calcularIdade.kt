5c1a21949e98a5abd66bd3f2be3de77389ec0624
Laisla Stephanie <82535016+LaislaSte@users.noreply.github.com>
2022-03-11 16:05:33 -0300

    update

--- src/main/kotlin/calcularIdade.kt
+++ src/main/kotlin/calcularIdade.kt
@@ -1,13 +1,15 @@
 //15. Receba data de nascimento de uma pessoa (AA) sem século.
 //Calcular a sua idade atual. Considere que não haja idade >70
 fun main(args: Array<String>) {
-    println("insira seu dia de nascimento:")
-    var dia:Int = readLine()!!.toInt()
-    println("insira seu mês de nascimento:")
-    var mes:Int = readLine()!!.toInt()
-    var meses = 1..12
-    for (i:Int ; i = 0 i < mes ){
+    println("insira seu ano de nascimento:")
+    var ano:Int = readLine()!!.toInt()
+    println("insira o ano atual:")
+    var anoAtual:Int = readLine()!!.toInt()
+
+    var idade:Int = anoAtual - ano
+    if (idade < 70){
+        println(idade)
+    }else{
 
     }
-
 }
