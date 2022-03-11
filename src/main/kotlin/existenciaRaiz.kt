48565df508bb9e6434e7095b9bb1d0c388e2efe6
Laisla Stephanie <82535016+LaislaSte@users.noreply.github.com>
2022-03-11 16:07:00 -0300

    update

--- src/main/kotlin/existenciaRaiz.kt
+++ src/main/kotlin/existenciaRaiz.kt
@@ -1,6 +1,7 @@
 //13.Receber 3 coeficientes A, B, C de uma equação da forma Ax²+Bx+c=0,
 // verifique a existência de raízes e caso exista, calcule e mostre.
 fun main(args: Array<String>) {
+    println("insira os coeficientes a, b e c respectivamente:")
     var a:Double = readLine()!!.toDouble()
     var b:Double = readLine()!!.toDouble()
     var c:Double = readLine()!!.toDouble()
@@ -10,14 +11,15 @@ fun main(args: Array<String>) {
 
     delta = (b*b)+(-4 * (a * c))
 
-    fun sqrt(delta:Double): Double{
-        return delta
+    if (delta != 0.0 && delta < 0.0){
+        fun sqrt(delta: Double): Double {
+            return delta
+        }
+        x1 = -b + sqrt(delta) / 2 * a
+        x2 = -b - sqrt(delta) / 2 * a
+
+        println("X1: " + x1 + " X2: " + x2)
     }
-
-    x1 = -b + sqrt(delta)/ 2 * a
-    x2 = -b - sqrt(delta)/ 2 * a
-
-    println("X1: "+ x1 + " X2: "+ x2 )
 
 }
 
