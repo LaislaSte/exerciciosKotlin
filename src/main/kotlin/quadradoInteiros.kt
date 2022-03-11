190d9e07bf34433d0ff189e920235c0215193638
Laisla Stephanie <82535016+LaislaSte@users.noreply.github.com>
2022-03-11 16:08:02 -0300

    update

--- src/main/kotlin/quadradoInteiros.kt
+++ src/main/kotlin/quadradoInteiros.kt
@@ -1,12 +1,11 @@
 //18. Calcule e mostre o quadrado dos números inteiros compreendidos entre 10 e 150.
 import kotlin.math.sqrt
 fun main(args: Array<String>) {
-    var c = [10..100]
-    var contador:Int = 0
-    var i = 0.0
-    while (contador > 10 && contador < 150){
-        i = sqrt(i)
-        println(i)
-        contador++
+    var c = 10..100
+    var contador = 0
+    for (contador in c){
+        var e = contador.toDouble()
+        e = sqrt(e)
+        println(e)
     }
 }
